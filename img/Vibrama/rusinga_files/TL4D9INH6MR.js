331f5ab92d895437117c3f7a418fee891d6fe985
uhqlA <alfie.alphonce@gmail.com>
2023-05-09 11:19:13 +0300

    logo added

--- img/Vibrama/rusinga_files/TL4D9INH6MR.js
+++
@@ -1,3 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("parseHeaders",[],(function(a,b,c,d,e,f){var g=/\r?\n[\t]+/g,h=/\r?\n/;function a(a){a=a.replace(g," ");var b={};a.split(h).forEach(function(a){a=a.split(":");var c=a.shift().trim();if(c){a=a.join(":").trim();b[c.toLowerCase()]=a}});return b}f["default"]=a}),66);
